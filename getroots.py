cda41820f1cd74928bd7c38ed55f9c7bb6222a79
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-28 11:59:37 +0530

    py labwork

---
+++ getroots.py
@@ -0,0 +1,14 @@
+print ("Enter tyhe value for a , b and c in euation , ax^2 + bx + c")
+a = int(input("value of a : "))
+b = int(input("value of b : "))
+c = int(input("value of c : "))
+root1 = round ( ((-b + (b**2 - 4*a*c)** (1/2))/(2*a)) , 2)
+root2 = round ((-b - (b**2 - 4*a*c)** (1/2))/(2*a),2)
+d = (b**2 - 4*a*c)
+if (d < 0) :
+    print("both the roots are complex")
+elif (d >= 0 and(root1==root2) ) :
+    print(f"both roots are real and equal i.e {root1}")
+    
+elif(d > 1 and (root1 != root2)):
+    print(f"both roots are real and distinct, roots are {root1} and {root2}")
