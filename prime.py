10bfe02a63d50e12009fda3f18c5a9b25149b687
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-28 12:51:54 +0530

    modified prime number file

--- prime.py
+++ prime.py
@@ -1,4 +1,12 @@
 user_input = int(input("Enter your number : "))
 count = 0
-for i in range (user_input) :
-    if ( user_input >= 2 and )
+if (user_input <= 1):
+    print("Not Prime!")
+else :
+    for i in range (1,user_input+1) :
+        if(user_input%i==0):
+            count += 1
+    if(count > 2) :
+        print("Not prime!")
+    else :
+        print("Prime!")        
