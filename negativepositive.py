449d6bd8824ef1eba1e6f70b5004245aac251c48
MAYANK2454-19 <mk2599599@gmail.com>
2026-01-28 12:00:40 +0530

    py labwork

---
+++ negativepositive.py
@@ -0,0 +1,7 @@
+user_input = int(input("Enter your number : "))
+if(user_input > 0) :
+    print("Your number is positive!")
+if(user_input == 0) :
+    print("Your number is ZERO!")
+if(user_input < 0) :
+    print("Your number is negative!")
